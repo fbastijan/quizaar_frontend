18174788e43b21c03bad9bd82e9c86634c3679b9
fbastijan <fbastijan@unipu.hr>
2025-06-04 17:22:12 +0200

    initial commit 2

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <div class="">
+      <Button>CLICK ME SHAD</button>
+  
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
+<script>
+import { Button } from "@/components/ui/button"
+export default {
+  components: {
+    Button,
+  },
+}
+
+
+</script>
